36b66233888ce4af20cc457f359bcb301d338f37
Dusan Stevanovic <duledu24@gmail.com>
2022-06-11 11:50:08 +0200

    add some jq

--- js/main.js
+++ js/main.js
@@ -2,14 +2,25 @@
 
 
 
-$(window).on('scroll', function() {
-  $(".section2").each(function() {
-    if (isScrolledIntoView($(this))) {
-      $(this).find(".aios-scroll-id-hp-33").addClass("nav-active-section");
-    
-    }
-  });
-});
+// $(window).on('scroll', function() {
+//   $(".section2").each(function() {
+//     if (isScrolledIntoView($(this))) {
+//       $(this).find(".aios-scroll-id-hp-33").addClass("nav-active-section");
+//
+//     }
+//   });
+// });
+//
+// function isScrolledIntoView(elem) {
+//     var docViewTop = $(window).scrollTop();
+//     var docViewBottom = docViewTop + $(window).height();
+//
+//     var elemTop = $(elem).offset().top;
+//     var elemBottom = elemTop + $(elem).height();
+//
+//     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+// }
+
 
 function isScrolledIntoView(elem) {
     var docViewTop = $(window).scrollTop();
@@ -20,3 +31,12 @@ function isScrolledIntoView(elem) {
 
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
+
+$(window).scroll(function () {
+    $('.textbox').each(function () {
+        if (isScrolledIntoView(this) === true) {
+            $(this).addClass('visible');
+        }
+    });
+
+});
