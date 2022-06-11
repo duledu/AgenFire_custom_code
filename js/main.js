671a28e7a8c543c1ea9bcfc9211b3fef771ced09
Dusan Stevanovic <duledu24@gmail.com>
2022-06-11 11:34:41 +0200

    add first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+
+
+$(window).on('scroll', function() {
+  $(".section2").each(function() {
+    if (isScrolledIntoView($(this))) {
+      $(this).find(".aios-scroll-id-hp-33").addClass("nav-active-section");
+    
+    }
+  });
+});
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
